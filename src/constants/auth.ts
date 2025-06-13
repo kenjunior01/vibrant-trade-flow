3bb3b1bb263e7b3f8f02b02cf3a712cbba82e86a
kenjunior01 <99036375+kenjunior01@users.noreply.github.com>
2025-06-13 15:22:27 +0000

    Limpeza: remoção de arquivos e diretórios desnecessários, mantendo apenas estrutura Django + React

--- src/constants/auth.ts
+++
@@ -1,7 +0,0 @@
-export const FLASK_API_URL = 'http://127.0.0.1:8000/api'; // URL do Django
-
-export const AUTH_STORAGE_KEYS = {
-  TOKEN: 'auth_token',
-  REFRESH_TOKEN: 'refresh_token',
-  USER_DATA: 'user_data',
-} as const;
--- src/pages/Index.tsx
+++
@@ -1,9 +0,0 @@
-
-import React from 'react';
-import { TradingPlatform } from '../components/TradingPlatform';
-
-const Index = () => {
-  return <TradingPlatform />;
-};
-
-export default Index;
